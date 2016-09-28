31d63a2ce24dbeb87a7beed0ca43363c5ae859a4
Matija Marohnić <matija.marohnic@gmail.com>
2016-09-28 13:54:01 +0200

    Use AVA instead of Jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import test from 'ava';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+test('renders without crashing', t => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
